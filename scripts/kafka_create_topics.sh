e870f50baf8e5312a10bf18ac2582cd5ac3d2432
dyrewucktv <mikolaj@roguski.eu>
2024-01-05 15:46:16 +0100

    dodane ładowanie rssów na nifi i skrypt do tworzenia topiców na kafce ( nie testowany )

---
+++ scripts/kafka_create_topics.sh
@@ -0,0 +1,29 @@
+TOPICS=("nifierror" "yfinance" "rss-ingestion-errors")
+KAFKA_TOPICS="/usr/local/kafka/bin/kafka-topics.sh"
+BOOTSTRAP_SERVER="node1:9092"
+
+
+if ! command -v $KAFKA_TOPICS &> /dev/null ; then
+  echo "${KAFKA_TOPICS} does not exist"
+  exit 1
+fi
+
+existing_topics=$(${KAFKA_TOPICS} --bootstrap-server ${BOOTSTRAP_SERVER} --list)
+
+
+for topic in "${TOPICS[@]}"; do
+  if ${KAFKA_TOPICS} --bootstrap-server ${BOOTSTRAP_SERVER} --topic "${topic}" --describe &> /dev/null; then
+    echo "topic ${topic} already exists!"
+  else
+    ${KAFKA_TOPICS} --bootstrap-server ${BOOTSTRAP_SERVER} --topic "${topic}" --create --partitions 1 --replication-factor 1
+    echo "creating topic ${topic}"
+  fi
+done
+
+for existing_topic in "${existing_topics[@]}"; do
+  if [[ ! " ${TOPICS[*]} " =~ ${existing_topic} ]]; then
+    echo "deleting topic ${existing_topic}"
+    ${KAFKA_TOPICS} --bootstrap-server ${BOOTSTRAP_SERVER} --topic "${existing_topic}" --delete
+  fi
+done
+
